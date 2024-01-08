52654824b5a36ff70b95b55632852851bcde2779
Sloth Service <155435374+slothservice@users.noreply.github.com>
2024-01-08 16:28:03 +0800

    plot algo result

--- nash.py
+++ nash.py
@@ -1,10 +1,31 @@
 import nashpy as nash
 import numpy as np
+import matplotlib.pyplot as plt
+
+def plot_strategy(d_strategy, w_strategy, eu_d, eu_w):
+    fig, axes = plt.subplots(2, 2, sharey='row')
+    x = np.arange(len(d_strategy))
+
+    # Plot each vector as a bar chart in the subplots
+    axes[0, 0].bar(x, d_strategy)
+    axes[0, 0].set_title('Depositor\'s strategy')
+
+    axes[0, 1].bar(x, w_strategy)
+    axes[0, 1].set_title('Withdrawer\'s strategy')
+
+    axes[1, 0].bar(x, eu_d)
+    axes[1, 0].set_title('Depositor\'s Expected Payoff')
+
+    axes[1, 1].bar(x, eu_w)
+    axes[1, 1].set_title('Withdrawer\'s Expected Payoff')
+
+    plt.tight_layout()
+    plt.savefig("algo.png")
 
 if __name__ == "__main__":
     payoff_depositor = []
     payoff_withdrawer = []
-    size =5
+    size = 9
     for i in range(size):
         d = i + 1
         payoff_depositor.append([])
@@ -22,3 +43,6 @@ if __name__ == "__main__":
     print(deposit_game)
     d_strategy, w_strategy = deposit_game.lemke_howson(initial_dropped_label =0)
     print(d_strategy, w_strategy)
+    eu_depositor = np.matmul(payoff_depositor, w_strategy)
+    eu_withdrawer = np.matmul(d_strategy, payoff_withdrawer)
+    plot_strategy(d_strategy, w_strategy, eu_depositor, eu_withdrawer)
